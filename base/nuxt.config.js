4df7fb6b2a7f45f176dce55316c69554b433c749
Nikke Leskelä <nileskel@gmail.com>
2019-05-13 09:00:33 +0000

    Updated default nuxt.config.js

--- base/nuxt.config.js
+++ base/nuxt.config.js
@@ -11,6 +11,9 @@ export default {
   server: {
     host: '0.0.0.0',
   },
+  head: {
+    meta: [{ charset: 'utf-8' }, { name: 'viewport', content: 'width=device-width, initial-scale=1' }],
+  },
   // prettier-ignore
   plugins: [
     '@/plugins/vuetify-init',
