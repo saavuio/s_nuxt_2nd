9d89a236154956860e91697083a976a83abc37af
Nikke Leskelä <nileskel@gmail.com>
2019-09-04 13:35:14 +0300

    Remove missing plugin from nuxt.config.js

--- base/nuxt.config.js
+++ base/nuxt.config.js
@@ -14,10 +14,6 @@ export default {
   head: {
     meta: [{ charset: 'utf-8' }, { name: 'viewport', content: 'width=device-width, initial-scale=1' }],
   },
-  // prettier-ignore
-  plugins: [
-    '@/plugins/vuetify-init',
-  ],
   build: {
     extend(config, ctx) {
       // NOTE: s_nuxt_2nd specific. Normally HardSource doesn't need to be configured.
