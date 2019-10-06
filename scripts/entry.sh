aa905cf068a5bb9b0a8bc678a44f63c10cf5efac
Nikke Leskelä <nileskel@gmail.com>
2019-10-06 21:42:44 +0300

    Fix entry.sh

--- scripts/entry.sh
+++ scripts/entry.sh
@@ -3,9 +3,10 @@ cd /$S_BASE_NAME
 
 # copy external node modules
 cp -a /ext/node_modules/* node_modules
+cp -a /ext/node_modules/.bin/* node_modules/.bin/
 
 # merge external package.json
-package-json-merge ./package.json /ext/package.json > /tmp/package.json
+jq -s 'reduce .[] as $d ({}; . *= $d)' ./package.json /ext/package.json > /tmp/package.json
 cat /tmp/package.json > ./package.json
 
 ${@:1}
