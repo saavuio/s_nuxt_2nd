c9f5bf0540cbfaad63d0b1ffdeb68d47eda01628
Nikke Leskelä <nileskel@gmail.com>
2019-10-06 21:52:45 +0300

    Silence cp error.

--- scripts/entry.sh
+++ scripts/entry.sh
@@ -3,7 +3,8 @@ cd /$S_BASE_NAME
 
 # copy external node modules
 cp -a /ext/node_modules/* node_modules
-cp -a /ext/node_modules/.bin/* node_modules/.bin/
+cp_silent () { cp -a $1 $2 2>/dev/null ; return 0 ; }
+cp_silent /ext/node_modules/.bin/* node_modules/.bin/
 
 # merge external package.json
 jq -s 'reduce .[] as $d ({}; . *= $d)' ./package.json /ext/package.json > /tmp/package.json
