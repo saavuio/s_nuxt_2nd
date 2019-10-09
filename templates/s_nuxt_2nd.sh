2a52dee97957a3e5c86846edb912ca15693159cb
Nikke Leskelä <nileskel@gmail.com>
2019-10-09 13:09:36 +0300

    Update node to 10.16.3. Update s_nuxt_2nd.sh template.

--- templates/s_nuxt_2nd.sh
+++ templates/s_nuxt_2nd.sh
@@ -4,6 +4,14 @@ cd $SCRIPT_DIR
 
 NAME=s_nuxt_2nd
 VERSION=v2
+IMAGE=saavu-local/${NAME}_${VERSION}
+
+if [ "$(docker images -q $IMAGE 2> /dev/null)" = "" ] && [ ! -f .reset-initiated ]; then
+  echo "Image missing, reset."
+  touch .reset-initiated
+  rm -f .ejected
+  rm -rf s_base/${NAME}
+fi
 
 if [ ! -d ./s_base/${NAME} ]; then
   mkdir -p s_base/${NAME}
@@ -24,6 +32,7 @@ if [ "$GIT_SHA_SHOULD_BE" != "LATEST" ]; then
     echo "..."
     sleep 3
     ./s_base/${NAME}/init.sh $GIT_SHA_SHOULD_BE
+    rm -f .reset-initiated
   fi
 fi
 
